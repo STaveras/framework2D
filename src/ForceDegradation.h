4d6d8d1ceaeeaf26c9ae0f9327bbc14154ca7731
Stanley Taveras <staveras@fullsail.edu>
2019-01-28 01:19:28 -0500

    - Finished the force degradation operator

--- src/ForceDegradation.h
+++ src/ForceDegradation.h
@@ -4,18 +4,21 @@
 #include "GameObject.h"
 #include "Timer.h"
 
-// All good things come to an end... (Or does it?)
+// All good things come to an end... (Or does they/it?)
 class ForceDegradationOperator : public ObjectOperator
 {
+   float _force;
    float _degradeTime;
    GameObject *_object;
-   State* _state; // the original state
+   GameObject::GameObjectState* _state;
    Timer _timer;
 
 public:
    ForceDegradationOperator(float time, GameObject *targetObject):_degradeTime(time)
    {
       _object = targetObject;
+      _state = targetObject->getState();
+      _force = _state->getForce();
       _timer.ResetElapsed();
    }
 
@@ -24,12 +27,24 @@ protected:
 
       if (_object == object) {
 
-         _timer.Update();
-         
-         //_timer.GetDeltaTime
+         if (_state == _object->GetCurrentState()) {
 
-         if (_timer.GetElapsedTime() >= _degradeTime)
-            return false;
+            _timer.Update();
+
+            float scalar = 1.0f - (_timer.GetDeltaTime() / _degradeTime);
+
+            _state->setForce(_force * scalar);
+
+            if (_timer.GetElapsedTime() >= _degradeTime)
+            {
+               // Original force; should we just set to 0, instead...?
+               _object->getState()->setForce(_force);
+
+               return false;
+            }
+         }
+         else
+            _timer.ResetElapsed();
       }
       return true;
    }
