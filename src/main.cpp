9cfb2e6df9dde2fed7088f766974255a81b12a09
Stanley Taveras <stan.taveras@gmail.com>
2022-04-09 15:21:33 -0400

    Revert "- Git stuff"
    This reverts commit 2c20c530023fd3c378e6f50d6afd20014cbd3db0.

--- src/main.cpp
+++ src/main.cpp
@@ -15,6 +15,14 @@
 
 //#include "SDSParser.h"
 
+#include "FollowObjectOperator.h"
+// #include "MaxVelocityOperator.h"
+#include "ApplyVelocityOperator.h"
+#include "UpdateRenderableOperator.h"
+#include "UpdateBackgroundOperator.h"
+
+#include "AnimationUtils.h"
+
 #include "FlappyTurd.h"
 
 #ifdef _WIN32
